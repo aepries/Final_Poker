6cf6282d0a5b27e45c4ae1f53bd2339020bfffeb
aaronpries <ae.pries@gmail.com>
2016-11-29 23:48:23 -0700

    Fixed Hand errors, just need to get the tester working.

--- src/Hand.java
+++ src/Hand.java
@@ -1,11 +1,38 @@
+import java.util.*;
 
 public class Hand {
 
+   public String[] array;
+    ArrayList<String> cards = new ArrayList<String>();
 
-    public ArrayList() cards = ;
+    public Hand(String[] array) {
+        cards.addAll(Arrays.asList(array));
+    }
 
-    ArrayList<String> h = new ArrayList<String>();
 
+    public void addCard(String card) {
+        cards.add(card);
+    }
 
+    public void removeCard(int position) {
+        String chosenCard = cards.get(position);
+        cards.remove(position);
+    }
 
-}
+    public void removeCard(String card) {
+        cards.remove(card);
+    }
+
+    public void gameStats() {
+    }
+
+    //figure this shit out
+    public void printHand(){
+        System.out.println(cards.size() + " cards:" + Arrays.asList(cards));
+        for (String s: cards){
+            s.charAt(0) =
+            System.out.println(Card.class.getName());
+        }
+        }
+    }
+
